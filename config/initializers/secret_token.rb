0f1b2fc071ef83acd9690c7ef987595656d82a81
Rafael Iga <rafael.f.iga@gmail.com>
2012-08-06 18:32:34 -0300

    selenium tests

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Selenium::Application.config.secret_token = '368172a66dc2b82804bec65bd9b37304c14a31eb21c03b9d6634aa4f22b8be9174b0ff46e270016934c9e501602557ecea4096abcbff415ba6ade4f71c2168dc'
---
+++ spec/integration/home_spec.rb
@@ -0,0 +1,19 @@
+require 'spec_helper'
+
+describe 'home page' do
+  it 'welcomes the user', :js => true do
+    visit '/'
+    page.should have_content('Welcome')
+  end
+end
+
+# describe 'blog post page' do
+#   it 'lets the user post a comment', :js => true do
+#     visit blog_post_path(blog_posts(:first_post).id)
+#     fill_in 'Author', :with => 'J. Random Hacker'
+#     fill_in 'Comment', :with => 'Awesome post!'
+#     click_on 'Submit'  # this be an Ajax button -- requires Selenium
+#     page.should have_content('has been submitted')
+#     page.should have_content('Awesome post!')
+#   end
+# end
